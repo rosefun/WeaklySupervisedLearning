051369e23176e884e4a42ebab96aa12281dbceed
rosefun <760751795@qq.com>
2020-11-22 16:25:30 +0800

    v0.0.1

---
+++ weaklysupervised/__init__.py
@@ -0,0 +1,7 @@
+"""
+The :mod:`.weakly supervised` module implements weakly-supervised learning
+algorithms. These algorithms utilized noisy labeled data for classification tasks.
+"""
+
+
+__all__ = ['QN_S3VM', 'S3VM', 'LabelPropagation']
